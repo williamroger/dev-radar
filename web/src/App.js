436c67cb41027562102cfb3b705f0abe7c415f47
William Roger <williamroger.frontend@gmail.com>
2020-01-25 18:25:54 -0300

    Finalizado toda a aplicação web com React

--- web/src/App.js
+++ web/src/App.js
@@ -1,109 +1,43 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
+
 import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
-function App() {
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords;
-        console.log('lati ', latitude);
-        console.log('long ', longitude);
-      },
-      (err) => {
-        console.log(err);
-      },
-      {
-        timeout: 30000,
-      }
-    );
-  }, []);
+import DevForm from './components/DevForm';
+import DevItem from './components/Devitem';
 
+function App() {
+  const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+      
+      setDevs(response.data);
+    }
+    
+    loadDevs();
+  }, []);
+  
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data);
+
+    setDevs([...devs, response.data]);
+  }
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required/>
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required />
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latidude">Latitude</label>
-              <input name="latidude" id="latidude" required />
-            </div>
-            <div className="input-block">
-              <label htmlFor="longetude">Longetude</label>
-              <input name="longetude" id="longetude" required />
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/3818294?s=460&v=4" alt="William Roger"/>
-              <div className="user-info">
-                <strong>William Roger</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>
-              Desenvolvimento da aplicação devRadar acompanhando a Semana OmniStack 10.0 da Rocketseat.
-            </p>
-            <a href="https://github.com/williamroger">Acessar perfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/3818294?s=460&v=4" alt="William Roger" />
-              <div className="user-info">
-                <strong>William Roger</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>
-              Desenvolvimento da aplicação devRadar acompanhando a Semana OmniStack 10.0 da Rocketseat.
-            </p>
-            <a href="https://github.com/williamroger">Acessar perfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/3818294?s=460&v=4" alt="William Roger" />
-              <div className="user-info">
-                <strong>William Roger</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>
-              Desenvolvimento da aplicação devRadar acompanhando a Semana OmniStack 10.0 da Rocketseat.
-            </p>
-            <a href="https://github.com/williamroger">Acessar perfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/3818294?s=460&v=4" alt="William Roger" />
-              <div className="user-info">
-                <strong>William Roger</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>
-              Desenvolvimento da aplicação devRadar acompanhando a Semana OmniStack 10.0 da Rocketseat.
-            </p>
-            <a href="https://github.com/williamroger">Acessar perfil no Github</a>
-          </li>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
         </ul>
       </main>
     </div>
